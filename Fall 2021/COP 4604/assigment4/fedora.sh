6b0159700298329c2a054daa857209a5b2143c92
Hector R <github.hector1@gmail.com>
2021-09-29 21:43:22 -0400

    Added Fedora Section for Assigment 4

---
+++ Fall 2021/COP 4604/assigment4/fedora.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$1" -le 0 ];
+    then
+        echo "Unable to run fibonacci sequence with numbers of 0 or less"
+    else
+        count=$(($1 - 2))
+        fib1=0
+        fib2=1
+        echo -n "$fib1, $fib2"
+        for _ in $(seq 1 "$count");
+        do
+            fib3=$((fib1 + fib2))
+            fib1=$fib2
+            fib2=$fib3
+            echo -n ", $fib3"
+        done 
+fi
+echo ""
