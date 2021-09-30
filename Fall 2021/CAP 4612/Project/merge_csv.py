1d741a31e4208c526806961cbbeb0467524fc12a
Hector R <github.hector1@gmail.com>
2021-09-30 10:36:29 -0400

    Further work on merging data sets

--- Fall 2021/CAP 4612/Project/merge_csv.py
+++ Fall 2021/CAP 4612/Project/merge_csv.py
@@ -3,27 +3,41 @@ import pandas as pd;
 import os
 import random as rd
 import threading as th
+import asyncio
 
 training_df = pd.DataFrame();
 testing_df = pd.DataFrame()
 random = rd.Random()
 threads = []
 
-def loadToMem(df: pd.DataFrame, fileName: str):
+lock = asyncio.Lock()
+async def loadToMem(df: pd.DataFrame, fileName: str):
+    testing = pd.DataFrame()
+    training =  pd.DataFrame()
     for i,row in df.iterrows():
         picked = random.randrange(0,100)
         if picked < 20:
-            testing_df.append(row)
+            testing = testing.append(row)
         else:
-            training_df.append(row)
-    print(f'File: {fileName} DONE!')
-                
+            training = training.append(row)
+    await lock.acquire()
+    # Lock out other threads to prevent race conditions
+    # Prvent data loss when overwritting global dataframe from multiple threads
+    try: 
+        global training_df
+        global testing_df
+        training_df = training_df.append(training)
+        testing_df = testing_df.append(testing)
+    finally:
+        lock.release()
+        print(f'File: {fileName} DONE!')
+    
 for file in os.listdir(os.getcwd()):
-    print(f'Working on File: {file}')
     if(file.endswith('.csv')):
+        print(f'Working on File: {file}')
         file_df = pd.read_csv(file)
         print(f'Loaded Entries (Rows): {len(file_df.index)}')
-        thread = th.Thread(target=lambda: loadToMem(file_df, file))
+        thread = th.Thread(target=lambda: asyncio.run(loadToMem(file_df, file)))
         threads.append(thread)
         thread.start()
 
@@ -32,8 +46,8 @@ for thread in threads:
     thread.join()
                
 print('Filling in empty spaces with "N/A"')        
-training_df.fillna(value='na', axis=0,inplace=True)
-testing_df.fillna(value='na', axis=0,inplace=True)
+training_df = training_df.fillna(value='na', axis=0,inplace=True)
+testing_df = testing_df.fillna(value='na', axis=0,inplace=True)
 print(f'Training Data # Rows: {len(training_df.index)}')
 training_df.to_csv('training_merged.csv')
 print(f'Testing Data # Rows: {len(testing_df.index)}')
