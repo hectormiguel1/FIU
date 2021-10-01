4e93c8f1938ae6d0d388fb93e221e763c80aa56e
Hector R <github.hector1@gmail.com>
2021-09-30 21:41:46 -0400

    Further work optimizing fedora.sh

--- Fall 2021/COP 4604/assigment4/fedora.sh
+++ Fall 2021/COP 4604/assigment4/fedora.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+if [ $# -eq 0 ]; then echo "No arguments passed"; exit; fi 
+
 if [ "$1" -le 0 ];
     then
         echo "Unable to run fibonacci sequence with numbers of 0 or less"
@@ -6,7 +9,8 @@ if [ "$1" -le 0 ];
         count=$(($1 - 2))
         fib1=0
         fib2=1
-        echo -n "$fib1, $fib2"
+        if [ $1 -ge 1 ]; then echo -n $fib1; fi
+        if [ $1 -ge 2 ]; then echo -n ", $fib2"; fi
         for _ in $(seq 1 "$count");
         do
             fib3=$((fib1 + fib2))
