8ba1c9fbecf5b8997529947c142fec58453a0210
Hector R <github.hector1@gmail.com>
2021-11-20 12:08:21 -0500

    Done with LR

--- Fall 2021/CAP 4612/HW3/linear_regression.py
+++ Fall 2021/CAP 4612/HW3/linear_regression.py
@@ -1,224 +1,73 @@
 import pandas as pd
 import numpy as np
 import matplotlib as plt
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+
 
 labels = pd.read_csv('MNIST_LABEL.csv')
 features = pd.read_csv('MNIST_15_15.csv')
 
-class LinearRegression:
+def solveLinearRegresion(X, y):
     """
-        A simple class to perform a task of Linear Regression.
-        
-        Steps
-        -----
-        * Find the hypothesis using y = mX + c, where X is as input vector.
-        * Find the cost value.
-        * Use gradient descent to update each parameters.
+        Finds the optimal B given the data X and Y
     """
-    def __init__(self):
-        """
-            This method doesnot take any initial attributes. You will tune the attributes later using methods.
-        """
-        # instantiate train data and label
-        self.X = None 
-        self.y = None
-        # to store all costs, initially cost is infinity
-        self.costs = [np.inf]
-        
-        # m is for len(X)
-        self.m = None
-        
-        # learning rate
-        self.alpha = None
-        
-        # our all parameters(from each iterations), m, c in y = mx + c
-        self.all_parameters = []
-    
-    def hypothesis(self, x):
-        """
-            A method to perform linear operation(mx + c) and return.
-            
-            Formula:
-            --------
-            \begin{equation}
-            h{_\theta}{(x)} = {\theta}^{T}x = \theta{_0} + \theta{_1} x_1 
-            \end{equation}
+    return np.dot(np.dot(np.linalg.pinv(np.dot(X.transpose(), X)), X.transpose()),y)
 
-        """
-        # y = XM, where X is of shape (M, N) and M of (N, 1)
-        h = np.dot(x, self.parameters)
-        return h
-    
-    def cost(self, yp, yt):
-        """
-            yp: Predicted y.
-            yt: True y.
+def predict(X, b, threshold):
+    """
+        Determines which classification X is using B based on some threshold
+    """
+    return np.array(np.dot(X, b) > threshold)
 
-            Formula:
-            -------
-            \begin{equation}
-            J{(\theta)} = \frac{1} {2m} \sum_{i=1}^m (h_\theta (x^{(i)}) - y^{(i)})^2
-            \end{equation}
+def accuracy(X, y):
+    """
+        Determines how many we guessed correctly
+    """
+    to_int = np.vectorize(lambda x: int(x))
+    return (sum(to_int(X) == to_int(y)) / float(len(y))) * 100
 
-        """
-        # find actual deviation
-        d = yp - yt 
-        # constant 1/2m
-        c = 1/(2 * self.m)
-        # finally, sum the square of gradients 
-        delta = c * np.sum(d ** 2)   
-        return delta
-    
-    def gradient_descent(self):
-        """
-            A method to perform parameter update based on gradient descent algorithm.
-            
-            Rule:
-            -----
-            \begin{equation}
-            \theta_j = \theta_j - \alpha \frac{1}{m} \sum_{i=1}^m (h_\theta (x^{(i)}) - y^{(i)}) x_j^{(i)}
-            \end{equation}
+def min_max_norm(X):
+    """
+        Simple min-max norm to normalize the data. Not really sure this is needed but got an extra 2% in my GD accuracy.
+    """
+    return X / 255.0
 
-        """
-        temp_theta = self.parameters
-        # for each theta
-        for j in range(len(temp_theta)):
-            grad = np.sum((self.hypothesis(self.X) - self.y)*np.array(self.X[:,j]).reshape(self.m, 1))
-            temp_theta[j] = self.parameters[j] - (self.alpha / self.m) * grad
-        self.parameters = temp_theta
-        self.all_parameters.append(temp_theta.flatten())
-        
-    def predict(self, X):
-        """
-            A method to return prediction. Perform preprocessing if model was trained preprocessed data.
+def cost(X, y, b):
+    """
+        Calculates the Ordinary least Squares
+    """
+    return np.sum((np.dot(X,b) - np.array(y))**2)
 
-            Preprocessing:
-            -------------
-            X = (X - X.mean()) / X.std()
-        """
-        if self.preprocessed != True:
-            X = np.insert(X, 0, 1, axis=1)
-            X = self.normalize(X)
-        return self.hypothesis(X)
-    
-    def visualize(self, thing="cost"):
-        """
-            Visualise the plots.
-            Available thing:
-            ----------
-            i. cost: Cost value vs iteration
-            ii. param: Parameters vs iteration
-            iii. all: cost and param vs iteration
-        """
-        legend = ["Loss"]
-        if thing == "cost" or thing == "all":
-            plt.title("Loss vs step.")
-            plt.grid(True)
-            plt.plot(self.costs)
-            plt.xlabel("Iterations")
-            plt.ylabel("Loss")
-            plt.legend(legend)
-    
-    #def visualize_parameters(self):
-        if thing == "param" or thing == "all":
-            plt.title("Parameter on each step.")
-            plt.grid(True)
-            plt.plot(self.all_parameters)
-            plt.xlabel("Iterations")
-            plt.ylabel("Parameters")
-            l = [fr"$\Theta{i}$" for i in range(len(self.parameters))]
-            if thing == "all":
-                l.insert(0, legend[0])
-                legend = l
-            else:
-                legend = l
-            plt.legend(legend)
-        
-    def normalize(self, X):
-        """
-            X: Input training data.
-            Returns: normalized x.
-            
-            Normalization:
-            --------------
-            X = (X - X.mean()) / X.std()
-        
-        """
-        
-        means, stds = [], []
-        normalized_x = X.copy()
-        # since we need to work with only the column we will iterate over shape[1]
-        for col in range(normalized_x.shape[1]):
-            means.append(normalized_x[:, col].mean())
-            stds.append(normalized_x[:, col].std())
-            if not col: continue
-            normalized_x[:, col] = (normalized_x[:, col] - means[-1])/stds[-1]
-        # store the means and stds. We need them on future.
-        self.means = means
-        self.stds = stds
-        return normalized_x
+def GD_LR(X, y, b):
+    """
+        Calculates gradient descient linear regression
+    """
+    return -np.dot(X.transpose(), y) + np.dot(np.dot(X.transpose(), X), b)
 
-    def fit(self, X, y, error_threshold = 0.001, preprocessed = True, alpha=0.01, show_every=100, iterations=1500):
-        """
-            X: input train (m X n),  if is not normalized and added axis for bias, use preprocessed=False.
-            y: train label (n X 1)
-            error_threshold: How much error is tolerable?
-            preprocessed: does train data has added axis for bias and normalized?
-            alpha: learning rate(update step)
-            show_every: how often to show cost?
-            iterations: how many steps to run weight update(gradient descent)?
-        """
-        
-        self.preprocessed = preprocessed
-        # if data already have bias added and normalized, leave it.
-        if preprocessed!=True:
-            X = np.insert(X, 0, 1, axis=1)
-            X = self.normalize(X)
-        self.X = X
-        self.y = y
-        self.alpha = alpha
-        
-        # how many of training examples are there?
-        self.m = len(X)
-        
-        # how many of parameters?
-        # initialize it to 0, shape must be (num. features + 1, 1)
-        # X here is normalized i.e it already have axis for bias
-        self.parameters = np.zeros((X.shape[1], 1))
-        costs = [np.inf] #Used to plot cost as function of iteration
-        i = 0
-        
-        # if our update is not done for iterations and error is pretty high, 
-        while (iterations>i and costs[-1]>error_threshold):
-            # find the cost value
-            cost_value = self.cost(self.hypothesis(self.X), self.y)
-            costs.append(cost_value)
-            # perform gradient descent and update param
-            self.gradient_descent() 
-            if i % show_every == 0:
-                print(f"Step: {i} Cost: {round(cost_value, 4)}.")
-            
-            i+=1
-        self.costs = costs
+normalized_features = min_max_norm(features)
+X_train, X_test, y_train, y_test = train_test_split(normalized_features, labels, test_size=0.3, random_state=42)
+labels = labels.replace([5,6], [0,1])
+b_opt = solveLinearRegresion(X_train, y_train)
 
+kfold = KFold(n_splits=10, shuffle=True)
+tprs = []
+fprs = []
+accuracies = []
+for train_index, test_index in kfold.split(normalized_features,labels):
+    y_train_kfolds = labels.iloc[train_index]
+    y_test_kfolds = labels.iloc[test_index]
+    X_test_kfolds = normalized_features.iloc[test_index]
+    X_train_kfolds = normalized_features.iloc[train_index]
+    b_kfold =solveLinearRegresion(X_train_kfolds, y_train_kfolds)
+    predictions_kfold = predict(X_test_kfolds, b_kfold,0.5)
+    accuracies.append(accuracy(predictions_kfold, y_test_kfolds))
+    tp, fn, fp, tn = confusion_matrix(y_test_kfolds,predictions_kfold,labels=[0,1]).reshape(-1)
+    tprs.append(tp/(tp+fn))
+    fprs.append(fp/(fp+tn))
 
-normalized_features = features / 255.0
-# b_opt = linear_regression(normalized_features, labels)
-# predictions = predict(features, b_opt, .5)
-skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=1)
-_, number_features = normalized_features.shape
-lr = LinearRegression()
-predictions = []
-for train_index, test_index in skf.split(normalized_features, labels):
-    X_train, X_test = normalized_features.iloc[train_index], normalized_features.iloc[test_index]
-    y_train, y_test = labels.iloc[train_index], labels.iloc[test_index]
-    lr.fit(X_train.to_numpy(), y_train.to_numpy().ravel(), error_threshold=0.5)
-    prediction = lr.predict(X_test.to_numpy())
-    prediction_rounded = np.round(prediction)
-    accuracy = (prediction_rounded == y_test.to_numpy()).sum() / len(y_test)
-    print(f'Accuracy: {accuracy}')
-    (tp, fp, fn, tn) = confusion_matrix(y_test.to_numpy(), prediction_rounded)
-    print(prediction)
-    print(f'TP: {tp}, FP: {fp}, FN: {fn}, TN: {tn}')
+print(f'TPRs: {tprs}')
+print(f'FPRs: {fprs}')
+print(f"Average TPR: {np.mean(tprs)}, Average FPR: {np.mean(fprs)}")
+print(f"Average Accuracy: {np.mean(accuracies)}")
